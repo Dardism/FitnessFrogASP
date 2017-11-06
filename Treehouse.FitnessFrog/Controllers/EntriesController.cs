d5d97400bcd897f05a0079629b106f0a6654f59a
Michael <dardismm@gmail.com>
2017-11-06 16:17:36 -0500

    added entries controller Add POST Action method

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -9,59 +9,81 @@ using Treehouse.FitnessFrog.Models;
 
 namespace Treehouse.FitnessFrog.Controllers
 {
-    public class EntriesController : Controller
-    {
-        private EntriesRepository _entriesRepository = null;
+  public class EntriesController : Controller
+  {
+    private EntriesRepository _entriesRepository = null;
 
-        public EntriesController()
+      public EntriesController()
+      {
+          _entriesRepository = new EntriesRepository();
+      }
+
+      public ActionResult Index()
+      {
+          List<Entry> entries = _entriesRepository.GetEntries();
+
+          // Calculate the total activity.
+          double totalActivity = entries
+              .Where(e => e.Exclude == false)
+              .Sum(e => e.Duration);
+
+          // Determine the number of days that have entries.
+          int numberOfActiveDays = entries
+              .Select(e => e.Date)
+              .Distinct()
+              .Count();
+
+          ViewBag.TotalActivity = totalActivity;
+          ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+          return View(entries);
+      }
+
+      public ActionResult Add()
+      {
+        if (Request.HttpMethod == "POST") 
         {
-            _entriesRepository = new EntriesRepository();
+        
+        }
+        else 
+        {
+              
         }
 
-        public ActionResult Index()
+          return View();
+      }
+
+      [ActionName("Add"), HttpPost]
+      public ActionResult AddPost() {
+        if (Request.HttpMethod == "POST") 
         {
-            List<Entry> entries = _entriesRepository.GetEntries();
 
-            // Calculate the total activity.
-            double totalActivity = entries
-                .Where(e => e.Exclude == false)
-                .Sum(e => e.Duration);
+        }
+        else {
 
-            // Determine the number of days that have entries.
-            int numberOfActiveDays = entries
-                .Select(e => e.Date)
-                .Distinct()
-                .Count();
-
-            ViewBag.TotalActivity = totalActivity;
-            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
-
-            return View(entries);
         }
 
-        public ActionResult Add()
-        {
-            return View();
-        }
+        return View();
+      }
 
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+      public ActionResult Edit(int? id)
+      {
+          if (id == null)
+          {
+              return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+          }
 
-            return View();
-        }
+          return View();
+      }
 
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
+      public ActionResult Delete(int? id)
+      {
+          if (id == null)
+          {
+              return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+          }
 
-            return View();
-        }
-    }
+          return View();
+      }
+  }
 }
