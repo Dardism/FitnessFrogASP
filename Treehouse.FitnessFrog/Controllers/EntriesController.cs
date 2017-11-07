98c068c4f3de7835ce326efb9e05a5f3f808dea0
Michael <dardismm@gmail.com>
2017-11-07 10:22:31 -0500

    Updated the add entry view to use HTML helper methods

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -57,13 +57,6 @@ namespace Treehouse.FitnessFrog.Controllers
       public ActionResult Add(DateTime? date, int? activityId, double? duration, 
                               Entry.IntensityLevel? intensity, bool? exclude, string notes) 
       {
-        ViewBag.Date = date;
-        ViewBag.ActivityId = activityId;
-        ViewBag.Duration = duration;
-        ViewBag.Intensity = intensity;
-        ViewBag.Exclude = exclude;
-        ViewBag.Notes = notes;
-
         return View();
       }
 
