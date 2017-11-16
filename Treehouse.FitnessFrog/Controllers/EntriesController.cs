69c2801a56d5f619527e35259d4ac8e1d010925a
Michael <dardismm@gmail.com>
2017-11-15 19:06:30 -0500

    Added delete view markup and functionality

--- Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -118,7 +118,23 @@ namespace Treehouse.FitnessFrog.Controllers
           return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
       }
 
-      return View();
+      //rereive entry for the provided id parameter value
+      Entry entry = _entriesRepository.GetEntry((int)id);
+
+      if (entry == null) 
+      {
+        return HttpNotFound();
+      }
+      //Pass the entry to the view
+      return View(entry);
+    }
+
+    [HttpPost]
+    public ActionResult Delete(int id) 
+    {
+      //delete the entry
+      _entriesRepository.DeleteEntry(id);
+      return RedirectToAction("Index");
     }
 
     private void ValidateEntry(Entry entry) 
